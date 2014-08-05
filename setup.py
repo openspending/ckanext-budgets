a3c5062f6dfa2a8d20ba4b8be4a47fff56c6188d
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-08-05 09:13:45 +0000

    Initialize CKAN extension structure for the budget data package creator
    This is the default CKAN extension structure as outputted by the
    paster create function:
    
        paster --plugin=ckan create -t ckanext ckanext-budgetdatapackage

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+    name='ckanext-budgetdatapackage',
+    version=version,
+    description="CKAN extension for creating and distributing budget data packages",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Tryggvi Bjorgvinsson',
+    author_email='tryggvi.bjorgvinsson@okfn.org',
+    url='',
+    license='AGPLv3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.budgetdatapackage'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        # myplugin=ckanext.budgetdatapackage.plugin:PluginClass
+    ''',
+)
