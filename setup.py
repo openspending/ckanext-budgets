81fcf532816cdbaa95070ad355e9f1e7ac76c08a
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-10-28 12:04:48 +0000

    Minor version 0.3.0
    * Update same dataset in OpenSpending when adding resource to CKAN

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.0'
+version = '0.3.0'
 
 description = "CKAN extension for creating/distributing budget data packages"
 with open('README.rst') as readme:
